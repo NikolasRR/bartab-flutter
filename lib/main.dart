37201f140926b7df81d95d0c8a919dfdd5f68160
Nikolas <nkls99@gmail.com>
2022-10-18 20:44:39 -0300

    homeScreen mvp

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,13 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PreferredSize(
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          flexibleSpace: Padding(padding: EdgeInsets.all(10), ),
-        ), preferredSize: const Size.fromHeight(70));
+    return AppBar(
+      backgroundColor: const Color.fromRGBO(139, 69, 19, 1),
+      title: const Text("BarTab",
+          style:
+              TextStyle(color: Color.fromRGBO(255, 215, 0, 1), fontSize: 40)),
+      centerTitle: true,
+    );
   }
 }
 
@@ -36,14 +38,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(238, 232, 170, 1),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: const <Widget>[
+          Header(),
+          SizedBox(height: 30,),
           Text(
             "Welcome to BarTab",
             style: TextStyle(
               fontSize: 30,
-              color: Colors.yellow,
+              color: Color.fromRGBO(249, 86, 54, 1),
             ),
           ),
           SizedBox(height: 20),
@@ -51,23 +56,27 @@ class HomePage extends StatelessWidget {
             "The app to help divide the bar tab with your friends!",
             style: TextStyle(
               fontSize: 20,
-              color: Colors.pink,
+              color: Color.fromRGBO(249, 86, 54, 1),
             ),
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 80),
+          
           Text(
-            "first, create a table!",
+            "1.first, create a table \n 2. second, add the participants \n 3. finally, tell us the itens!",
             style: TextStyle(
+              height: 2,
               fontSize: 20,
-              color: Colors.pink,
+              color: Color.fromRGBO(249, 86, 54, 1),
             ),
             textAlign: TextAlign.center,
           ),
+          SizedBox(height: 20,),
           IconButton(
             onPressed: null,
             icon: Icon(
               Icons.add_box_rounded,
+              color: Color.fromRGBO(249, 86, 54, 1),
             ),
             iconSize: 150,
           )
